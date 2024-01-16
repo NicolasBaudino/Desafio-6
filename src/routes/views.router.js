52420f7492492188dbf10c2214cda52b9139d19e
Baudino Nicolas <baudino.nicolas@hotmail.com>
2024-01-16 15:31:53 -0300

    Arreglando errores

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -11,7 +11,15 @@ router.get("/products", async (req,res) => {
     const { limit, page, query, sort } = req.query;
     const products = await productDao.findProducts(limit, page, query, sort);
 
-    res.render("products.hbs", {products})
+    const userData = req.session.user;
+    const welcomeMessage = 'Bienvenido';
+
+    res.render("products.hbs", { products, user: userData, welcomeMessage }, (err, html) => {
+        if (err) {
+            throw err
+        }
+        res.send(html)
+    })
 })
 
 router.get("/carts/:id", async (req,res) => {
